3c1a7e5787710c9a3253d393b8b46dac6a0de200
zhzhang <dcatfly@gmail.com>
2019-10-24 01:00:15 +0800

    fix count & better code

--- 2019.11.11/taobao.js
+++ 2019.11.11/taobao.js
@@ -5,8 +5,8 @@ const appName = "手机淘宝";
 const WAIT_TIME = 23000;
 // 逛20个店铺
 let maxCount = 20;
-// 除了逛店铺外的四个任务
-let maxOther = 4;
+// 除了逛店铺外的几个任务
+let maxOther = 6;
 
 setScreenMetrics(1440, 3040);
 
@@ -19,31 +19,25 @@ btn.click();
 textContains("待兑换红包").waitFor();
 click(1230, 2300);
 
-const worker = (fn, max) => {
+const loopWorker = (fn, loop) => {
   let count = 0;
-  while (count < max) {
+  while (count < loop) {
     fn();
+    count = count + 1;
   }
 };
 
-const enterShopTask = () => {
-  const taskBtn = textContains("去进店");
+const getTask = text => () => {
+  const taskBtn = textContains(text);
   taskBtn.waitFor();
   taskBtn.click();
   sleep(WAIT_TIME);
   back();
+  sleep(1000);
 };
 
-const goExploreTask = () => {
-  const taskBtn = textContains("去浏览");
-  taskBtn.waitFor();
-  taskBtn.click();
-  sleep(WAIT_TIME);
-  back();
-};
+loopWorker(getTask("去进店"), maxCount);
 
-worker(enterShopTask, maxCount);
-
-worker(goExploreTask, maxOther);
+loopWorker(getTask("去浏览"), maxOther);
 
 device.cancelKeepingAwake();
