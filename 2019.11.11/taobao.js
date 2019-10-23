61cfeb1370acb1708c47def3293b38acb4bcf295
zhzhang <dcatfly@gmail.com>
2019-10-23 21:20:45 +0800

    change taobao

--- 2019.11.11/taobao.js
+++ 2019.11.11/taobao.js
@@ -1,71 +1,62 @@
-
 auto();
-var appName = "手机淘宝";
-var maxCount = 20;
-var maxOther = 4;
-
 device.keepScreenDim();
-setScreenMetrics(1440,3040);
-//openAppSetting(getPackageName(appName));
-// var closeBth = textContains("强制停止");
-// closeBth.waitFor();
-// closeBth.click();
-// textContains(appName).waitFor();
-// click(1158,1124);
-// id("button1").waitFor();
 
-//while(!click("强制停止")){};
+const appName = "手机淘宝";
+const WAIT_TIME = 23000;
+let maxCount = 20;
+let maxOther = 4;
+let count = 0;
+
+setScreenMetrics(1440, 3040);
 
 launchApp(appName);
 
-//toastLog("qqq");
-var btn = desc("捉猫猫");
+const btn = desc("捉猫猫");
 btn.waitFor();
 btn.click();
 
 textContains("待兑换红包").waitFor();
-click(1230,2300);
+click(1230, 2300);
 
-var count = 0;
-var taskBtn1 = textContains("去进店");
+const worker = fn => {
+  const timer = setInterval(() => {
+    fn(timer);
+  }, WAIT_TIME);
+};
 
-events.observeToast();
-events.onToast(function(){
-  console.log("toast");
-})
-function a(){
-  //  while(count<maxCount && taskBtn1.exist){
+const enterShopTask = () => {
+  const taskBtn1 = textContains("去进店");
   taskBtn1.waitFor();
   taskBtn1.click();
-  //  descContains("完成").waitFor();
-  //   back();
   count = count + 1;
-  // }
-}
+};
 
-setInterval(function(){
-  if(count!==0){back();}
-  a();
-},22000);
-
-count = 0;
-var taskBtn2 = textContains("去浏览");
-
-function b(){
-  // while(count < maxOther){
+const goExploreTask = () => {
+  const taskBtn2 = textContains("去浏览");
   taskBtn2.waitFor();
   taskBtn2.click();
-  // desc("任务完成").waitFor();
-  //  back();
   count = count + 1;
-  // }
-}
-function c(){
-  setInterval(function(){
-    if(count !== 0){back();};
-    b();
-  }, 20000)
-
 };
+
+worker(timer => {
+  count !== 0 && back();
+
+  if (count < maxCount) {
+    enterShopTask();
+  } else {
+    count = 0;
+    clearInterval(timer);
+    worker(_timer => {
+      count !== 0 && back();
+      if (count < maxOther) {
+        goExploreTask();
+      } else {
+        clearInterval(_timer);
+        back();
+        back();
+      }
+    });
+  }
+});
 
 device.cancelKeepingAwake();
